7a5f7d40f6a800d83f454fc8e2abc800a27b5a2e
marcosfsp <marcosfsp25@gmail.com>
2024-10-24 22:53:10 -0300

    segundo commit

---
+++ flutterapi/lib/api_service.dart
@@ -0,0 +1,19 @@
+import 'dart:convert';
+
+import 'package:flutterapi/post.dart';
+import 'package:http/http.dart' as http;
+
+class ApiService {
+  final String baseUrl = 'https://jsonplaceholder.typicode.com/posts';
+
+  Future<List<Post>> getPosts() async {
+    final response = await http.get(Uri.parse(baseUrl));
+    if (response.statusCode == 200) {
+      List<dynamic> body = jsonDecode(response.body);
+      return body.map((dynamic item) {
+        return Post.fromMap(item);
+      }).toList();
+    }
+    return [];
+  }
+}
---
+++ flutterapi/lib/pagina.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutterapi/post.dart';
+
+class Pagina extends StatefulWidget {
+  const Pagina({super.key});
+
+  @override
+  State<Pagina> createState() => _PaginaState();
+}
+
+
+
+class _PaginaState extends State<Pagina> {
+  List<Post> posts = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView.builder(
+        itemCount: posts.length,
+        itemBuilder: (context, index){
+          return ListTile(
+            title: Text('${posts[index].userId}'),
+          );
+        }
+        ),
+    );
+  }
+}
